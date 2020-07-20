2fc74280582c9b3e1a74b856bae5f7fbb9228494
Khris B <khris@0xd7.net>
2020-07-20 19:48:06 -0400

    Temporarily disabled https

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://0xd7.net'
+SITEURL = 'http://0xd7.net'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
